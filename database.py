c5c61d59f03d706710bb0fdd32891c167c592f1f
jeancarlosfaveros0103-cpu <jeancarlos.faveros0103@gmail.com>
2026-06-24 11:34:36 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,25 @@
-@app.route("/registro", methods=["GET", "POST"])
-def registro():
+import sqlite3
 
+DB_NAME = "jarbas.db"
+
+def conectar():
+    return sqlite3.connect(DB_NAME)
+
+def criar_tabela():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        senha TEXT NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
     if request.method == "POST":
 
         try:
